b37e8c490c1d1a585a9f08d9facb4ed1aec081d1
Mackenzie Salloum <mackenziesalloum@gmail.com>
2018-12-22 17:51:11 -0800

    Fix formatting.

--- screensaver.py
+++ screensaver.py
@@ -23,22 +23,22 @@ class App():
         self.root.after(5000, self.update_weather)
 
     def get_weather(self):
-      print("Updating weather")
-      api_key = "f5988e28703d9fdcb99201c66e8eabcf"
-      city = "Kelowna"
-      response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key)
+        print("Updating weather")
+        api_key = "f5988e28703d9fdcb99201c66e8eabcf"
+        city = "Kelowna"
+        response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key)
 
-      res_json = json.loads(response.content.decode('utf-8'))
+        res_json = json.loads(response.content.decode('utf-8'))
 
-      current_temp = int(round(res_json["main"]["temp"] - 273.15, 0))
-      current_weather = res_json["weather"][0]["main"]
-      low_temp = int(res_json["main"]["temp_min"] - 273.15)
-      high_temp = int(res_json["main"]["temp_max"] - 273.15)
-      weather_text = "Weather: " + current_weather + ", " + str(current_temp) + " °C\n"
-      low_temp_text = "Low: " + str(low_temp) + " °C\n"
-      high_temp_text = "High: " + str(high_temp) + " °C"
-      text = weather_text + low_temp_text + high_temp_text
-      self.label.configure(text=text + "\nNext bus: " + self.get_next_bus_departure_time())
+        current_temp = int(round(res_json["main"]["temp"] - 273.15, 0))
+        current_weather = res_json["weather"][0]["main"]
+        low_temp = int(res_json["main"]["temp_min"] - 273.15)
+        high_temp = int(res_json["main"]["temp_max"] - 273.15)
+        weather_text = "Weather: " + current_weather + ", " + str(current_temp) + " °C\n"
+        low_temp_text = "Low: " + str(low_temp) + " °C\n"
+        high_temp_text = "High: " + str(high_temp) + " °C"
+        text = weather_text + low_temp_text + high_temp_text
+        self.label.configure(text=text + "\nNext bus: " + self.get_next_bus_departure_time())
 
     #
     # Returns the next bus departure time as a string.
