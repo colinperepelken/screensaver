d6fafdfd422411c319f3d13407ad6f6a7400dfcb
Mackenzie Salloum <mackenziesalloum@gmail.com>
2018-12-22 17:03:58 -0800

    Combine files into one widget.

--- weather.py
+++ weather.py
@@ -1,7 +1,8 @@
 import requests
 import json
 from tkinter import *
-
+import googlemaps
+from datetime import datetime
 
 
 class App():
@@ -10,6 +11,7 @@ class App():
         self.label = Label(text="")
         self.label.pack()
         self.root.title("Weather")
+        self.root.geometry("700x300")
         self.update_weather()
         self.root.mainloop()
 
@@ -29,7 +31,28 @@ class App():
       low_temp = "Low: " + str(res_json["main"]["temp_min"] - 273.15)  + "\n"
       high_temp = "High: " + str(res_json["main"]["temp_max"] - 273.15)
       text = weather + low_temp + high_temp
-      self.label.configure(text=text)
+      self.label.configure(text=text + "\nNext bus: " + self.get_next_bus_departure_time())
+
+    #
+    # Returns the next bus departure time as a string.
+    # From the Artium Student Residence to UBCO.
+    # Next bus time relies on current time.
+    #
+    def get_next_bus_departure_time(self):
+
+        # Initialize Google Maps API.
+        gmaps = googlemaps.Client(key='AIzaSyCZ_3BKxqS5_SS41sUchUUfd6Sq4jiY6-A')
+
+        # Request directions via public transit.
+        now = datetime.now()
+        directions_result = gmaps.directions("The Artium Student Residence, Kelowna",
+                                            "UBC Okanagan, Kelowna, BC",
+                                            mode="transit",
+                                            departure_time=now)
+
+        # Retrieve departure time and return string.
+        return directions_result[0]['legs'][0]['departure_time']['text']
+
       
 
 app=App()
