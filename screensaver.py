f083cf0fbe3f3c2555610c7ff20cc208fc7310df
Parsa <ParsaRajabiPR@gmail.com>
2018-12-22 21:02:22 -0800

    added comments and current time

--- screensaver.py
+++ screensaver.py
@@ -1,4 +1,4 @@
-#importing
+# importing
 import requests
 import json
 from tkinter import Tk, Label
@@ -9,36 +9,54 @@ from datetime import datetime
 class App():
     def __init__(self):
         self.root = Tk()
+        # decalre labels
         self.weatherLabel = Label(text="")
         self.busLabel = Label(text="")
+        self.timeLabel = Label(text="")
+
+        # packs
         self.weatherLabel.pack(expand=True)
         self.busLabel.pack(expand=True)
+        self.timeLabel.pack(expand=True)
+
+        # root
         self.root.title("Live Screen")
         self.root.geometry("700x300")
         self.root.attributes("-fullscreen", True)
         self.root.configure(background="black")
+
+        # weather config
         self.weatherLabel.configure(fg="white", background="black")
         self.weatherLabel.configure(font=("Courier", 44))
+
+        # bus config
         self.busLabel.configure(fg="white", background="black")
         self.busLabel.configure(font=("Courier", 44))
+
+        # time config
+        self.timeLabel.configure(fg="white", backgorund="black")
+        self.timeLabel.configure(font=("Courier", 60))
+
         self.update()
         self.root.mainloop()
 
+    # update function
     def update(self):
         self.weatherLabel.configure(text=self.get_weather())
+        self.timeLabel.configure(text=self.get_current_time())
         self.busLabel.configure(text=self.get_next_bus_departure_time())
         self.root.after(5000, self.update)
 
+    # returns weather
     def get_weather(self):
         print("Updating weather")
         api_key = "f5988e28703d9fdcb99201c66e8eabcf"
         city = "Kelowna"
         response = {}
         try:
-          response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key)
+            response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key)
         except:
-          print("Exception occured while getting weather.")
-
+            print("Exception occured while getting weather.")
 
         res_json = json.loads(response.content.decode('utf-8'))
 
@@ -50,8 +68,13 @@ class App():
         low_temp_text = "Low: " + str(low_temp) + " °C\n"
         high_temp_text = "High: " + str(high_temp) + " °C"
         text = weather_text + low_temp_text + high_temp_text
-        return text        
-    #
+        return text
+
+        # returns current time
+
+    def get_current_time(self):
+        return "Current Time: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
     # Returns the next bus departure time as a string.
     # From the Artium Student Residence to UBCO.
     # Next bus time relies on current time.
@@ -64,12 +87,12 @@ class App():
         # Request directions via public transit.
         now = datetime.now()
         directions_result = gmaps.directions("The Artium Student Residence, Kelowna",
-                                            "UBC Okanagan, Kelowna, BC",
-                                            mode="transit",
-                                            departure_time=now)
+                                             "UBC Okanagan, Kelowna, BC",
+                                             mode="transit",
+                                             departure_time=now)
 
         # Retrieve departure time and return string.
         return "Next bus: " + directions_result[0]['legs'][0]['departure_time']['text']
- 
+
 
 app = App()
